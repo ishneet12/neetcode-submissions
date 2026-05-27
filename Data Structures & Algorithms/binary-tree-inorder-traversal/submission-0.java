a4fd430760bb9df7174ffe522d6f09249baaef72
Ishneet Kaur <ishneetkaur712@gmail.com>
2026-05-27 19:54:56 +0530

    Add: binary-tree-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-0.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+
+        if(root==null) return ans;
+
+        Stack<TreeNode> st = new Stack<>();
+
+        while(root!=null || !st.isEmpty()){
+
+            if(root!=null){
+                st.push(root);
+                root = root.left;
+            }
+
+            else{
+                if(st.isEmpty()) break;
+                root = st.pop();
+                ans.add(root.val);
+                root = root.right;
+            }
+        }
+        return ans;
+    }
+}
