47aa29fc8e86088075130e3e5a37e6814526735f
Ishneet Kaur <ishneetkaur712@gmail.com>
2026-08-09 11:45:40 +0530

    Add: append-characters-to-string-to-make-subsequence - submission-1

---
+++ Data Structures & Algorithms/append-characters-to-string-to-make-subsequence/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int i = 0 , j = 0;
+        int n = s.length() , m = t.length();
+
+        while(i<n && j<m){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+
+        return m-j;
+    }
+}
