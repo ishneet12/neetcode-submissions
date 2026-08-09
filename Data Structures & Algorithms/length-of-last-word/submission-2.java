8147a78d9d695d4530d9b209dcef1d531bfb3250
Ishneet Kaur <ishneetkaur712@gmail.com>
2026-08-09 11:53:10 +0530

    Add: length-of-last-word - submission-2

---
+++ Data Structures & Algorithms/length-of-last-word/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        int n = s.length();
+
+        int j = n-1;
+        int cnt = 0;
+
+        while(j>=0 && s.charAt(j)==' '){
+            j--;
+        }
+
+        while(j>=0 && s.charAt(j)!=' '){
+            cnt++;
+            j--;
+        }
+
+        return cnt;
+    }
+}
