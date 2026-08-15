7b822deb221781a79ce3f4eb9cff9cb71286dbe8
Ishneet Kaur <ishneetkaur712@gmail.com>
2026-08-15 10:13:06 +0530

    Add: squares-of-a-sorted-array - submission-0

---
+++ Data Structures & Algorithms/squares-of-a-sorted-array/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] sortedSquares(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            arr[i] = arr[i]*arr[i];
+        }
+
+        Arrays.sort(arr);
+        return arr;
+    }
+}
