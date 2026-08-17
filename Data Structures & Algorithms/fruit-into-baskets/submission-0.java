6d0b81cfd0b319e128cd3df638d286710d6e0dc7
Ishneet Kaur <ishneetkaur712@gmail.com>
2026-08-17 09:45:14 +0530

    Add: fruit-into-baskets - submission-0

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int totalFruit(int[] arr) {
+        HashMap<Integer,Integer> mp = new HashMap<>();
+        int j=0;
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            mp.put(arr[i],mp.getOrDefault(arr[i],0)+1);
+            if(mp.size()>2){
+                mp.put(arr[j],mp.getOrDefault(arr[j],0)-1);
+                if(mp.get(arr[j])==0){
+                    mp.remove(arr[j]);
+                }
+                j++;
+            }
+        }
+
+        return n-j;
+    }
+}
