f5549a5ad1770188b2f3cf544c50f7963ce0e84b
Ishneet Kaur <ishneetkaur712@gmail.com>
2026-08-23 10:54:32 +0530

    Add: single-element-in-a-sorted-array - submission-2

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int singleNonDuplicate(int[] arr) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (mid == 0 || mid == n - 1) {
+                return arr[mid];
+            }
+            if (arr[mid] != arr[mid - 1] && arr[mid] != arr[mid + 1]) {
+                return arr[mid];
+            }
+
+            else if (mid % 2 == 0 && arr[mid] == arr[mid + 1]
+                || mid % 2 != 0 && arr[mid] == arr[mid - 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
